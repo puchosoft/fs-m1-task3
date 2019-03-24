a5210a882f1d0629b82b9e8e949fe5f4360364f2
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-03-24 15:30:45 -0300

    Agregamos un script de bash

---
+++ git-init.sh
@@ -0,0 +1,14 @@
+echo
+echo Credenciales actuales
+echo ---------------------
+git config --global user.name
+git config --global user.email
+echo
+echo Remotos actuales
+echo ----------------
+git remote -v
+echo
+git config --global user.name "Jose Luis Gonzalez"
+git config --global user.email "puchosoft@gmail.com"
+echo
+
