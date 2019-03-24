4725fc2b2320354cd9d66efd01feab00f7fd68ac
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-03-24 18:23:37 -0300

    Agregamos scripts

---
+++ git-set.sh
@@ -0,0 +1,10 @@
+# Credenciales
+git config --global user.name "Jose Luis Gonzalez"
+git config --global user.email "puchosoft@gmail.com"
+
+# Manager de credenciales
+git config credential.helper ''
+
+# Remotos
+git remote rm origin
+git remote add github http://github.com/puchosoft/fs-m1-task3
