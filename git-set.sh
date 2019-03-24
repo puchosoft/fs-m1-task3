f94017c1bc610ddd9e40953a31f397f6ee870b72
Jose Luis Gonzalez <puchosoft@gmail.com>
2019-03-24 18:46:08 -0300

    ultimo cambio en scripts

--- git-set.sh
+++ git-set.sh
@@ -8,5 +8,4 @@ git config credential.helper ''
 # Remotos
 git remote rm origin
 git remote add github http://github.com/puchosoft/fs-m1-task3
-git config brach.master.remote github
-git config branch.master.merge=refs/heads/master
+git push --set-upstream github master
